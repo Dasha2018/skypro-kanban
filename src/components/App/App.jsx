60582f15b1c3ab6a69a015cb431e5dfece7bb18b
Dasha2018 <zavadskayadasha97@gmail.com>
2025-03-17 16:32:24 +0700

    Выполнила доработки в соотвествии с заданием в Props и хуки

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,22 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import UserProfile from "../popups/PopUser/PopUser.jsx";
+import cardList from "../../data.js"; // Импорт данных карточек
+import Column from "../Column/Column.jsx";
+
+const columns = [
+  "Без статуса",
+  "Нужно сделать",
+  "В работе",
+  "Тестирование",
+  "Готово",
+];
+
+// Группировка карточек по колонкам
+const groupedCards = columns.reduce((acc, status) => {
+  acc[status] = cardList.filter((card) => card.status === status);
+  return acc;
+}, {});
 
 function App() {
   return (
@@ -423,98 +441,23 @@ function App() {
                 >
                   <a href="#popNewCard">Создать новую задачу</a>
                 </button>
-                <a href="#user-set-target" className="header__user _hover02">
-                  Ivan Ivanov
-                </a>
-                <div
-                  className="header__pop-user-set pop-user-set"
-                  id="user-set-target"
-                >
-                  <p className="pop-user-set__name">Ivan Ivanov</p>
-                  <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-                  <div className="pop-user-set__theme">
-                    <p>Темная тема</p>
-                    <input
-                      type="checkbox"
-                      className="checkbox"
-                      name="checkbox"
-                    />
-                  </div>
-                  <button type="button" className="_hover03">
-                    <a href="#popExit">Выйти</a>
-                  </button>
-                </div>
+                <UserProfile />
               </nav>
             </div>
           </div>
         </header>
+
         <main className="main">
           <div className="container">
             <div className="main__block">
               <div className="main__content">
-                <div className="main__column column">
-                  <div className="column__title">
-                    <p>Без статуса</p>
-                  </div>
-                  <div className="cards">
-                    {/* Add cards here as needed */}
-                    {/* Example card */}
-                    <div className="cards__item">
-                      <div className="cards__card card">
-                        <div className="card__group">
-                          <div className="card__theme _orange">
-                            <p className="_orange">Web Design</p>
-                          </div>
-                          <a href="#popBrowse" target="_self">
-                            <div className="card__btn">
-                              <div></div>
-                              <div></div>
-                              <div></div>
-                            </div>
-                          </a>
-                        </div>
-                        <div className="card__content">
-                          <a href="" target="_blank">
-                            <h3 className="card__title">Название задачи</h3>
-                          </a>
-                          <div className="card__date">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="13"
-                              height="13"
-                              viewBox="0 0 13 13"
-                              fill="none"
-                            >
-                              <g clipPath="url(#clip0_1_415)">
-                                <path
-                                  d="M10.5625 2.03125H2.4375C1.7644 2.03125 1.21875 2.5769 1.21875 3.25V10.5625C1.21875 11.2356 1.7644 11.7812 2.4375 11.7812H10.5625C11.2356 11.7812 11.7812 11.2356 11.7812 10.5625V3.25C11.7812 2.5769 11.2356 2.03125 10.5625 2.03125Z"
-                                  stroke="#94A6BE"
-                                  strokeWidth="0.8"
-                                  strokeLinejoin="round"
-                                />
-                                <path
-                                  d="M11.7812 4.0625H1.21875M3.25 1.21875V2.03125V1.21875ZM9.75 1.21875V2.03125V1.21875Z"
-                                  stroke="#94A6BE"
-                                  strokeWidth="0.8"
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                />
-                              </g>
-                              <defs>
-                                <clipPath id="clip0_1_415">
-                                  <rect width="13" height="13" fill="white" />
-                                </clipPath>
-                              </defs>
-                            </svg>
-                            <p>30.10.23</p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    {/* Repeat similar card structure as needed */}
-                  </div>
-                </div>
-                {/* Repeat similar columns for "Нужно сделать", "В работе", "Тестирование", "Готово" */}
+                {columns.map((status) => (
+                  <Column
+                    key={status}
+                    title={status}
+                    cards={groupedCards[status] || []}
+                  />
+                ))}
               </div>
             </div>
           </div>
